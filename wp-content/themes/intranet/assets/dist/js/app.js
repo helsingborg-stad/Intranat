5b598cd4a7c8d8c7183c621de8c81aa698e04090
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-03-09 10:41:07 +0100

    Report ajax script

--- wp-content/themes/intranet/assets/dist/js/app.js
+++ wp-content/themes/intranet/assets/dist/js/app.js
@@ -434,6 +434,56 @@ Intranet.Misc.News = (function ($) {
 
 
 Intranet = Intranet || {};
+Intranet.Misc = Intranet.Misc || {};
+
+Intranet.Misc.ReportPost = (function ($) {
+
+    function ReportPost() {
+        $('.report-post').on('submit', function (e) {
+            e.preventDefault();
+
+            var $target = $(this),
+                data = new FormData(this);
+                data.append('action', 'report_post');
+
+            if (data.get('g-recaptcha-response') === '') {
+                return false;
+            }
+
+            $target.find('input[type="submit"]').hide();
+            $target.find('.modal-footer').append('<div class="loading"><div></div><div></div><div></div><div></div></div>');
+
+            $.ajax({
+                url: ajaxurl,
+                type: 'POST',
+                data: data,
+                dataType: 'json',
+                processData: false,
+                contentType: false,
+                success: function(response, textStatus, jqXHR) {
+                    if (response.success) {
+                        $('.modal-footer', $target).html('<span class="notice success"><i class="pricon pricon-check"></i> ' + response.data + '</span>');
+
+                    } else {
+                        $('.modal-footer', $target).html('<span class="notice warning"><i class="pricon pricon-notice-warning"></i> ' + response.data + '</span>');
+                    }
+                },
+                complete: function () {
+                    setTimeout(function() {
+                        location.hash = '';
+                    }, 3000);
+                }
+            });
+
+            return false;
+        });
+    }
+
+    return new ReportPost();
+
+})(jQuery);
+
+Intranet = Intranet || {};
 Intranet.Search = Intranet.Search || {};
 
 Intranet.Search.General = (function ($) {
