2537dd7dac967ba0c6052fa8eaf0f18d22e3cfb4
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-03-16 13:04:21 +0100

    Single group and comment views

---
+++ wp-content/themes/intranet/views/single-intranet-group.blade.php
@@ -0,0 +1,46 @@
+@extends('templates.master')
+
+@section('content')
+
+<div class="container main-container">
+    @include('partials.breadcrumbs')
+
+    <div class="grid">
+
+        @include('partials.sidebar-left')
+
+        <div class="{{ $contentGridSize }}">
+            @if (is_single() && is_active_sidebar('content-area-top'))
+                <div class="grid sidebar-content-area sidebar-content-area-top">
+                    <?php dynamic_sidebar('content-area-top'); ?>
+                </div>
+            @endif
+
+            <div class="grid">
+                <div class="grid-sm-12">
+                        @include('partials.blog.type.post-single')
+                </div>
+            </div>
+
+            @if ($isMember)
+                <div class="grid">
+                    <div class="grid-sm-12">
+                        @include('partials.blog.group-comments-form')
+                    </div>
+                </div>
+                @if(isset($comments) && ! empty($comments))
+                    <div class="grid">
+                        <div class="grid-sm-12">
+                            @include('partials.blog.comments')
+                        </div>
+                    </div>
+                @endif
+            @endif
+        </div>
+
+        @include('partials.sidebar-right')
+
+    </div>
+</div>
+
+@stop
