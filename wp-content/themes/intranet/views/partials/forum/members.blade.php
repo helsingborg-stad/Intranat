40f60fc5752b64c328b39f9a4d553ac66ad5ffb1
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-04-05 15:54:18 +0200

    Show join button if not member

--- wp-content/themes/intranet/views/partials/forum/members.blade.php
+++ wp-content/themes/intranet/views/partials/forum/members.blade.php
@@ -10,3 +10,8 @@
         @endforeach
     </div>
 </div>
+@if($isMember)
+	<div class="grid-xs-12">
+		<a href="#modal-invite-members" class="btn btn-block btn-sm gutter gutter-margin gutter-top" data-action="share-email"><i class="pricon pricon-group pricon-space-right"></i> <?php _e('Invite members', 'municipio-intranet'); ?></a>
+	</div>
+@endif
