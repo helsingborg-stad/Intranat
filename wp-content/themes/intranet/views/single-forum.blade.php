a5063fd06d846171fcd32726c6b1be1079a6a2c9
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-04-05 11:00:42 +0200

    New wrapper class

--- wp-content/themes/intranet/views/single-forum.blade.php
+++ wp-content/themes/intranet/views/single-forum.blade.php
@@ -10,30 +10,32 @@
         @include('partials.sidebar-left')
 
         <div class="{{ $contentGridSize }}">
-            @if (is_single() && is_active_sidebar('content-area-top'))
-                <div class="grid sidebar-content-area sidebar-content-area-top">
-                    <?php dynamic_sidebar('content-area-top'); ?>
-                </div>
-            @endif
-            <div class="grid">
-                <div class="grid-sm-12">
-                    @include('partials.blog.type.post-single')
-                </div>
-            </div>
-            @if ($isMember)
-                <div class="grid">
-                    <div class="grid-sm-12">
-                        @include('partials.blog.forum-comments-form')
-                    </div>
-                </div>
-                @if(isset($comments) && ! empty($comments))
-                    <div class="grid">
-                        <div class="grid-sm-12 forum-comments">
-                            @include('partials.blog.comments')
-                        </div>
+            <div class="box forum-box">
+                @if (is_single() && is_active_sidebar('content-area-top'))
+                    <div class="grid sidebar-content-area sidebar-content-area-top">
+                        <?php dynamic_sidebar('content-area-top'); ?>
                     </div>
                 @endif
-            @endif
+                <div class="grid">
+                    <div class="grid-sm-12">
+                        @include('partials.blog.type.post-single')
+                    </div>
+                </div>
+                @if ($isMember)
+                    <div class="grid">
+                        <div class="grid-sm-12">
+                            @include('partials.blog.forum-comments-form')
+                        </div>
+                    </div>
+                    @if(isset($comments) && ! empty($comments))
+                        <div class="grid">
+                            <div class="grid-sm-12 forum-comments">
+                                @include('partials.blog.comments')
+                            </div>
+                        </div>
+                    @endif
+                @endif
+            </div>
         </div>
 
         @include('partials.sidebar-right')
