04954fb2aeb1862bcfc4645c5386f961aecc080a
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-04-16 16:58:55 +0200

    Search preview

--- wp-content/mu-plugins/searchPreview.php
+++ wp-content/mu-plugins/searchPreview.php
@@ -1,1 +1,22 @@
 <?php
+
+add_filter('site_option_active_sitewide_plugins', 'modify_sitewide_plugins');
+
+function modify_sitewide_plugins($value) {
+
+    if(is_admin()) {
+        return false;
+    }
+
+    global $current_user;
+    if(!$current_user) {
+        return;
+    }
+
+    if (in_array($current_user->data->user_login, array("seno1000"))) {
+        unset($value['elasticpress/elasticpress.php']);
+        $value['algolia-frontend/algolia-frontend'] = time();
+    }
+
+    return $value;
+}
