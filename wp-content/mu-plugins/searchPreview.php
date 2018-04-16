d3a90fa7e7e046cb25a4696d1cbf0db585271570
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-04-16 17:04:46 +0200

    Search preview tester

--- wp-content/mu-plugins/searchPreview.php
+++ wp-content/mu-plugins/searchPreview.php
@@ -1,16 +1,16 @@
 <?php
 
-//add_filter('site_option_active_sitewide_plugins', 'modify_sitewide_plugins');
+add_filter('site_option_active_sitewide_plugins', 'modify_sitewide_plugins');
 
 function modify_sitewide_plugins($value) {
 
     if(is_admin()) {
-        return false;
+        return $value;
     }
 
     global $current_user;
     if(!$current_user) {
-        return;
+        return $value;
     }
 
     if (in_array($current_user->data->user_login, array("seno1000"))) {
