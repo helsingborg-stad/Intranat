848a9bd090f225a0e95a967b49330685499d2bf3
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-04-17 14:55:04 +0200

    Update allowed users to preview search

--- wp-content/mu-plugins/searchPreview.php
+++ wp-content/mu-plugins/searchPreview.php
@@ -1,22 +1,51 @@
 <?php
 
+global $allowedUsers;
+$allowedUsers  = array("seno1000", "hewi1000", "nira1000", "logr1002","joha1032");
+
 add_filter('site_option_active_sitewide_plugins', 'modify_sitewide_plugins');
 
 function modify_sitewide_plugins($value) {
-
-    if(is_admin()) {
-        return $value;
-    }
-
+global $allowedUsers;
     global $current_user;
-    if(!$current_user) {
-        return $value;
+
+    if(isset($_COOKIE) && is_array($_COOKIE)) {
+        foreach($_COOKIE as $key => $item) {
+            if(preg_match("/wordpress_logged_in/i", $key)) {
+                $uname = explode("|", $item);
+                if(isset($uname[0])) {
+                    $uname = $uname[0];
+                    break;
+                }
+            }
+        }
     }
 
-    if (in_array($current_user->data->user_login, array("seno1000"))) {
+    if (in_array($uname, $allowedUsers)) {
+        $value['algolia-frontend/algolia-frontend.php'] = time();
         unset($value['elasticpress/elasticpress.php']);
-        $value['algolia-frontend/algolia-frontend'] = time();
     }
 
     return $value;
 }
+
+add_filter('option_options_use_algolia_search', function($a) {
+global $allowedUsers;
+     if(isset($_COOKIE) && is_array($_COOKIE)) {
+        foreach($_COOKIE as $key => $item) {
+            if(preg_match("/wordpress_logged_in/i", $key)) {
+                $uname = explode("|", $item);
+                if(isset($uname[0])) {
+                    $uname = $uname[0];
+                    break;
+                }
+            }
+        }
+    }
+
+    if (in_array($uname, $allowedUsers)) {
+        return 1;
+    }
+
+    return $a;
+} );
