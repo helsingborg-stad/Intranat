dbafa8ffd0e35ed438490b1c9433672e64cf6271
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-06-23 15:19:38 +0200

    Move the algolia js rendering location

--- wp-content/themes/intranet/library/Search/Algolia.php
+++ wp-content/themes/intranet/library/Search/Algolia.php
@@ -7,7 +7,13 @@ class Algolia
 
     public function __construct()
     {
-        add_filter('algolia_should_index_searchable_post', array($this, 'indexPrivatePosts'), 10, 2);
+        //Algilia index js mod
+        add_filter('AlgoliaIndexJSSearchPage/ActionMountPoint', function() {
+            return 'loop_start';
+        }); 
+
+        //Algolia vendor fix (discontiniued plugin)
+        add_filter('algolia_should_index_searchable_post', array($this, 'indexPrivatePosts'), 10, 2); 
     }
 
     public function indexPrivatePosts($should_index, $post)
